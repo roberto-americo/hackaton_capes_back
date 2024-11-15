e9ca85105d31e555be3a3bdd101d23b841b8ba7d
Roberto Américo <americoguerras@gmail.com>
2024-11-15 10:36:05 +0000

    Project created

---
+++ src/main/java/com/americo/hackaton_capes_back/HackatonCapesBackApplication.java
@@ -0,0 +1,13 @@
+package com.americo.hackaton_capes_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonCapesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonCapesBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/americo/hackaton_capes_back/HackatonCapesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americo.hackaton_capes_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonCapesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
